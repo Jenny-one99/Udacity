98b521396eab96f533615af828ff68e92894aa86
Jenny Huang <hjjhyd@126.com>
2021-11-06 10:50:50 +0800

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,29 @@
+import json
+import sagemaker
+import base64
+from sagemaker.serializers import IdentitySerializer
+
+ENDPOINT='image-classification-2021-11-05-16-11-21-533'
+
+def lambda_handler(event, context):
+
+    # Decode the image data
+    image = base64.b64decode(event['image_data'])
+
+    # Instantiate a Predictor
+    predictor = sagemaker.predictor.Predictor(
+        ENDPOINT, 
+        sagemaker_session=sagemaker.Session())
+
+    # For this model the IdentitySerializer needs to be "image/png"
+    predictor.serializer = IdentitySerializer("image/png")
+    
+    # Make a prediction:
+    inferences = predictor.predict(image)
+    
+    # We return the data back to the Step Function    
+    event["inferences"] = inferences.decode('utf-8')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
